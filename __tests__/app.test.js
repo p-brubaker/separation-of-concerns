df9f31c6dead9d831d27809a7a81cb36d5e0b6ff
Paul Brubaker <pbrubak@gmail.com>
2021-09-22 17:43:21 -0700

    get all test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,8 +11,15 @@ jest.mock('twilio', () => () => ({
 }));
 
 describe('03_separation-of-concerns-demo routes', () => {
-    beforeEach(() => {
-        return setup(pool);
+    beforeEach(async () => {
+        await setup(pool);
+        return Promise.all(
+            [{ quantity: 12 }, { quantity: 100 }, { quantity: 3 }].map(
+                (order) => {
+                    return request(app).post('/api/v1/orders').send(order);
+                }
+            )
+        );
     });
 
     it('creates a new order in our database and sends a text message', () => {
@@ -22,9 +29,21 @@ describe('03_separation-of-concerns-demo routes', () => {
             .then((res) => {
                 // expect(createMessage).toHaveBeenCalledTimes(1);
                 expect(res.body).toEqual({
-                    id: '1',
+                    id: expect.any(String),
                     quantity: 10,
                 });
+            });
+    });
+
+    it('should get all orders', () => {
+        return request(app)
+            .get('/api/v1/orders')
+            .then((res) => {
+                expect(res.body).toEqual([
+                    { id: expect.any(String), quantity: 12 },
+                    { id: expect.any(String), quantity: 100 },
+                    { id: expect.any(String), quantity: 3 },
+                ]);
             });
     });
 });
